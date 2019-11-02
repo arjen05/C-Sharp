8a4be37b587afe31b729a654eb4b9cb5e8d3761a
Arjen Pangalina <55436257+arjen05@users.noreply.github.com>
2019-11-02 11:09:33 +1100

    Add files via upload

---
+++ ArithmeticOperation/ArithmeticOperation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArithmeticOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNumber = 10;
+            var secondNumber = 5;
+
+            //addition
+            int addition = firstNumber + secondNumber;
+            Console.WriteLine("{0} + {1} = " + addition, firstNumber, secondNumber);
+
+            //subtraction
+            int subtraction = firstNumber - secondNumber;
+            Console.WriteLine("{0} - {1} = " + subtraction, firstNumber, secondNumber);
+
+            //multiplication
+            int multiplication = firstNumber * secondNumber;
+            Console.WriteLine("{0} * {1} = " + multiplication, firstNumber, secondNumber);
+
+            //division
+            int division = firstNumber / secondNumber;
+            Console.WriteLine("{0} / {1} = " + division, firstNumber, secondNumber);
+
+        }
+    }
+}
